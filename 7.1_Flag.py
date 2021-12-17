b23b73bcaab7df2f50a8b9ee62d62d9040f84f58
thompsonlan <llthompson1207@gmail.com>
2021-12-17 10:16:27 -0600

    Finished cafe and hospital rooms

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -13,14 +13,11 @@ import arcade #
 arcade.open_window(494, 260, "Flag project")
 arcade.set_background_color((256, 256, 256))
 arcade.start_render()
-for y in range(10, 281, 40):
-    arcade.draw_line(0, y, 494, y, (191, 10, 48), 20)
+for y in range(10, 281, 40): arcade.draw_line(0, y, 494, y, (191, 10, 48), 20)
 arcade.draw_lrtb_rectangle_filled(0, 197.6, 260, 121, (0, 40, 104))
 for z in range(142, 260, 30):
-    for w in range(33, 165, 31):
-        arcade.draw_text("*", w, z, (255, 255, 255), 10)
+    for w in range(33, 165, 31): arcade.draw_text("*", w, z, (255, 255, 255), 10)
 for z in range(126, 260, 30):
-    for x in range(18, 195, 31):
-        arcade.draw_text("*", x, z, (255, 255, 255), 10)
+    for x in range(18, 195, 31): arcade.draw_text("*", x, z, (255, 255, 255), 10)
 arcade.finish_render()
 arcade.run()
